5e50a25bb4888e937fc70a4eb18780292752c36d
Dennis Zimmerer <zimmerer.dennis98@gmail.com>
2021-10-28 12:26:05 +0200

    settings

--- app/src/main/java/com/dis/logolink/gui/MainActivity.kt
+++ app/src/main/java/com/dis/logolink/gui/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.dis.logolink.gui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnContinue = findViewById<Button>(R.id.btn_continue)
+        val btnLevels = findViewById<Button>(R.id.btn_levels)
+        val btnProfiles = findViewById<Button>(R.id.btn_profiles)
+        val btnSettings = findViewById<Button>(R.id.btn_settings)
+
+        btnSettings.setOnClickListener{
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
+            Toast.makeText(this, "Clicked", Toast.LENGTH_LONG).show()
+        }
     }
 }
