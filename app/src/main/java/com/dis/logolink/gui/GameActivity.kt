7190a1fd94035259b9dd9eebe772418d7ce6b29b
b4stYgp <82322390+b4stYgp@users.noreply.github.com>
2021-11-09 16:05:36 +0100

    TestClasses
    Just for Testing

---
+++ app/src/main/java/com/dis/logolink/gui/GameActivity.kt
@@ -0,0 +1,14 @@
+package com.dis.logolink.gui
+
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class GameActivity(levelsCode: String) : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game)
+
+    }
+}
