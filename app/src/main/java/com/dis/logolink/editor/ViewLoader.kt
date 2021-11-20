c04ee628ba707ba4a9e0acb4c2572b80b9a03f01
b4stYgp <82322390+b4stYgp@users.noreply.github.com>
2021-11-20 19:54:11 +0100

    WIP createView with Constraints
    constraint bei der Erstellung des Button hinzufügen ooooooder später updaten

--- app/src/main/java/com/dis/logolink/editor/ViewLoader.kt
+++ app/src/main/java/com/dis/logolink/editor/ViewLoader.kt
@@ -5,12 +5,15 @@ import android.view.ViewGroup
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.LinearLayout
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.updateLayoutParams
 
 import com.dis.logolink.gui.R
 import com.dis.logolink.models.Component
 import com.dis.logolink.models.IdentityGate
 import com.dis.logolink.models.Level
 import com.dis.logolink.parser.Parser
+import kotlinx.android.synthetic.main.activity_level.*
 
 class ViewLoader() {
     lateinit var level: Level
@@ -21,6 +24,7 @@ class ViewLoader() {
                 ViewGroup.LayoutParams.WRAP_CONTENT,
                 ViewGroup.LayoutParams.WRAP_CONTENT)
         input.setImageResource(R.drawable.lamp_off)
+        var constraintLayout : ConstraintLayout = LevelLayout
         return input
     }
 
