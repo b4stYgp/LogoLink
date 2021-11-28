6dabed4c09b39f65f6b53220cfd933705e9086b9
Dennis Zimmerer <zimmerer.dennis98@gmail.com>
2021-11-28 20:34:42 +0100

    canvas paint methods: > EXCEPTION (l.34, CanvasLoader.kt)

--- app/src/main/java/com/dis/logolink/gui/CanvasLoader.kt
+++ app/src/main/java/com/dis/logolink/gui/CanvasLoader.kt
@@ -1,24 +1,35 @@
 package com.dis.logolink.gui
 
+import android.app.Activity
 import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.view.View
 
 class CanvasLoader(screenWidth: Int, screenHeight: Int) {
     val bitmap: Bitmap = Bitmap.createBitmap(screenWidth, screenHeight, Bitmap.Config.ARGB_8888)
     val canvas: Canvas = Canvas(bitmap)
+    var paint: Paint? = null
     lateinit var positions: MutableList<Float>
 
-    fun <T>calculateLinePositions(elementList: MutableList<T>, inputList: MutableList<T>, context: Context){
+    fun <T>calculateLinePositions(elementList: MutableMap<T, T>, activity: Activity): Bitmap{
         for(element in elementList){
-            setStartPosition(element as View)
+            setStartPosition(activity.findViewById(element.key as Int))
             //EndX EndY
-            //TODO:
-
+            setEndPostition(activity.findViewById(element.value as Int))
         }
+        paint = Paint().apply {
+            color = Color.BLACK
+            strokeWidth = 10F
+            strokeCap = Paint.Cap.SQUARE
+        }
+        CanvasPencil(canvas, paint!!).drawLines(positions)
+        return bitmap
     }
 
+    //EXCEPTION CAN'T GET RIGHT VAL (NULL)
     fun setStartPosition(element: View){
         //StartX
         positions.add(element.right.toFloat())
@@ -26,7 +37,10 @@ class CanvasLoader(screenWidth: Int, screenHeight: Int) {
         positions.add(element.right.toFloat() - element.height.toFloat())
     }
 
-    fun setEndPostition() {
-
+    fun setEndPostition(element: View) {
+        //End X
+        positions.add(element.left.toFloat())
+        //End Y
+        positions.add(element.left.toFloat() - element.height.toFloat())
     }
 }
